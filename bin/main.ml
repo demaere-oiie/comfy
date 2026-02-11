cbc51d1264cca638dea36d51b636b748647a681f
Da Def <da.def.lang.belta@gmail.com>
2026-02-11 20:47:31 +0100

    fac

--- bin/main.ml
+++ bin/main.ml
@@ -1,15 +1,19 @@
 open Comfy.Lib
 
 let r = Seq (Imp 'A', Seq (Rep (Imp 'B'), Imp 'C'))
+
+(****
 let s = Seq (Rep (Seq (Imp 'A', Imp 'B')), Imp 'C')
+let t = Seq (Rep (Imp 'B'), Imp 'C')
+
+let practv s =
+  print_endline "---";
+  List.iter print_endline
+    (List.map
+       (fun v -> match v with i, k -> Char.escaped i ^ " " ^ string_of_re k)
+       (sactv s))
+****)
 
 let () =
   print_endline @@ string_of_re r;
-  List.iter print_endline
-    (List.map
-       (fun v -> match v with i, k -> Char.escaped i ^ " " ^ string_of_re k)
-       (actv r));
-  List.iter print_endline
-    (List.map
-       (fun v -> match v with i, k -> Char.escaped i ^ " " ^ string_of_re k)
-       (actv s))
+  print_endline @@ string_of_int @@ run r 7
--- lib/lib.ml
+++ lib/lib.ml
@@ -25,3 +25,49 @@ let rec string_of_re s =
   | Rep x -> "Rep(" ^ string_of_re x ^ ")"
   | Alt (x, y) -> "Alt(" ^ string_of_re x ^ "|" ^ string_of_re y ^ ")"
   | Seq (x, y) -> "Seq(" ^ string_of_re x ^ ";" ^ string_of_re y ^ ")"
+
+module CharMap = Map.Make (Char)
+
+let f = Failure "fail"
+
+let imps =
+  CharMap.(
+    empty
+    |> add 'A' (fun v ->
+        match v with 1, n, _ -> (2, 1, n) | _, _, _ -> raise f)
+    |> add 'B' (fun v ->
+        match v with 2, a, n -> (2, a * n, n - 1) | _, _, _ -> raise f)
+    |> add 'C' (fun v ->
+        match v with 2, a, _ -> (1, a, 0) | _, _, _ -> raise f)
+    |> add '_' (fun v -> v))
+
+let doms =
+  CharMap.(
+    empty
+    |> add 'A' (fun v -> match v with 1, _, _ -> true | _, _, _ -> false)
+    |> add 'B' (fun v -> match v with 2, _, _ -> true | _, _, _ -> false)
+    |> add 'C' (fun v -> match v with 2, _, 0 -> true | _, _, _ -> false)
+    |> add '$' (fun v -> match v with 0, 1, 2 -> true | _, _, _ -> false)
+      (*TC?*)
+    |> add '_' (fun _ -> true))
+
+let pris = CharMap.(empty |> add 'A' 0 |> add 'B' 1 |> add 'C' 0 |> add '_' 3)
+
+let sactv s =
+  let scmp x y =
+    match x with
+    | i, _ -> ( match y with j, _ -> CharMap.(find i pris - find j pris))
+  in
+  List.sort scmp (actv s)
+
+let rec runx r s =
+  match r with Eps -> ( match s with _, n, _ -> n) | _ -> dalts s (sactv r)
+
+and dalts s zs =
+  match zs with
+  | [] -> raise (Failure "not exhaustive")
+  | (i, k) :: zs2 ->
+      if (CharMap.find i doms) s then runx k ((CharMap.find i imps) s)
+      else dalts s zs2
+
+let run s n = runx s (1, n, 0)
